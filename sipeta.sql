7f27edf24b9dae110b0526f4bab9c8eb4ab55c95
anargyaa <43247002+anargyaa@users.noreply.github.com>
2024-01-20 02:00:35 +0700

    Add files via upload

--- sipeta.sql
+++ sipeta.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 20, 2023 at 09:24 AM
+-- Generation Time: Jan 14, 2024 at 09:19 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,11 +29,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `konsultasi` (
   `id_konsultasi` int(11) NOT NULL,
-  `namaLengkap` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `telephone` int(15) NOT NULL,
-  `pesan` text NOT NULL
+  `id_user` int(11) NOT NULL,
+  `username_g` varchar(60) NOT NULL,
+  `email_g` varchar(60) NOT NULL,
+  `telephone_g` int(15) NOT NULL,
+  `topik` varchar(60) NOT NULL,
+  `pesan` text NOT NULL,
+  `waktu` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `konsultasi`
+--
+
+INSERT INTO `konsultasi` (`id_konsultasi`, `id_user`, `username_g`, `email_g`, `telephone_g`, `topik`, `pesan`, `waktu`) VALUES
+(1, 0, 'a', 'b@gmail.com', 2147483647, 'Persyaratan lainnya', 'TESTING', '2024-01-14 03:59:54'),
+(2, 3, 'Reffa', 'reffa.kaila@gmail.com', 8999, 'Cara Penggunaan Website', 'asd', '2024-01-14 04:00:27');
 
 -- --------------------------------------------------------
 
@@ -43,23 +54,25 @@ CREATE TABLE `konsultasi` (
 
 CREATE TABLE `perencanaan` (
   `id_perencanaan` int(11) NOT NULL,
-  `namaIntansi` varchar(255) NOT NULL,
-  `pimpinanIntansi` varchar(255) NOT NULL,
-  `peruntukan` varchar(255) NOT NULL,
-  `kategoriProyek` varchar(255) NOT NULL,
-  `kelurahan` varchar(255) NOT NULL,
-  `kecamatan` varchar(255) NOT NULL,
-  `koordinatLintang` varchar(10) NOT NULL,
-  `koordinatBujur` varchar(10) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama_i` varchar(60) NOT NULL,
+  `pimpinan_i` varchar(60) NOT NULL,
+  `peruntukan` varchar(60) NOT NULL,
+  `kategori_proyek` varchar(60) NOT NULL,
+  `kelurahan` varchar(60) NOT NULL,
+  `kecamatan` varchar(60) NOT NULL,
+  `koordinat_l` varchar(10) NOT NULL,
+  `koordinat_b` varchar(10) NOT NULL,
   `lokasi` text NOT NULL,
-  `rencataTataRuang` varchar(255) NOT NULL,
-  `perkiraanLuas` varchar(255) NOT NULL,
-  `perkiraanPanjang` varchar(255) NOT NULL,
-  `perkiraanAlokasi` varchar(255) NOT NULL,
-  `mulaiPersiapan` varchar(255) NOT NULL,
-  `mulaiPelaksanaan` varchar(255) NOT NULL,
-  `dokumenSumberData` varchar(255) NOT NULL,
-  `keterangan` text NOT NULL
+  `rencana_tr` varchar(60) NOT NULL,
+  `perkiraan_l` varchar(10) NOT NULL,
+  `perkiraan_p` varchar(10) NOT NULL,
+  `perkiraan_a` varchar(10) NOT NULL,
+  `m_persiapan` varchar(60) NOT NULL,
+  `m_pelaksanaan` varchar(60) NOT NULL,
+  `dokumen` varchar(60) NOT NULL,
+  `keterangan` text NOT NULL,
+  `status` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -69,21 +82,22 @@ CREATE TABLE `perencanaan` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `username` varchar(60) NOT NULL,
   `telepone` int(15) NOT NULL,
-  `email` varchar(255) NOT NULL,
+  `email` varchar(60) NOT NULL,
   `password` varchar(60) NOT NULL,
-  `role` enum('0','1') NOT NULL
+  `role` enum('admin','user') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `telepone`, `email`, `password`, `role`) VALUES
-(1, 'admin', 0, 'admin@gmail.com', '202cb962ac59075b964b07152d234b70', '0'),
-(3, 'Reffa Kaila', 0, 'reffa.kaila@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', '1');
+INSERT INTO `user` (`id_user`, `username`, `telepone`, `email`, `password`, `role`) VALUES
+(1, 'admin', 0, 'admin@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'admin'),
+(3, 'Reffa Kaila', 0, 'reffa.kaila@gmail.com', 'f5bb0c8de146c67b44babbf4e6584cc0', 'user'),
+(4, 'Kailaa', 2147483647, 'kaila@gmail.com', '7153735db2b2052c603ef3068c00d683', 'user');
 
 --
 -- Indexes for dumped tables
@@ -105,7 +119,7 @@ ALTER TABLE `perencanaan`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -115,19 +129,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `konsultasi`
 --
 ALTER TABLE `konsultasi`
-  MODIFY `id_konsultasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_konsultasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `perencanaan`
 --
 ALTER TABLE `perencanaan`
-  MODIFY `id_perencanaan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_perencanaan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
